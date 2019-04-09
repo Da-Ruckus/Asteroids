87c660a1b937d0906e8ad4eb2b6392d989c1dd96
djkulbeck <djkulbeck@Cisco-03.anc.local>
2019-04-09 10:44:56 -0700

    Space invaders

--- src/Ship.java
+++ src/Ship.java
@@ -2,8 +2,9 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.transform.Affine;
 import javafx.scene.transform.Rotate;
+//import main.Keyboard;
 
-public class Ship {
+public class Ship extends Keyboard {
 	
 	public Ship (){
 		
@@ -14,18 +15,55 @@ public class Ship {
 	
 	public void draw(GraphicsContext gc) {
 		
+		
 		gc.setFill(Color.AQUA);
 		gc.save();
-		gc.transform(new Affine(new Rotate(degrees, 150,150 )));
+		gc.transform(new Affine(new Rotate(degrees, shipX, shipY )));
 		
-		double X [] = {150,130,170};
-		double Y [] = {130,170,170};
+		double X [] = {shipX,shipX-20,shipX+20};
+		double Y [] = {shipY-20,shipY+20,shipY+20};
 		gc.fillPolygon(X, Y, 3);
 		gc.restore();
-		
-		
+	
 	}
 
+	double shipX = 150;
+	double shipY = 275;
+
+	int edge = 280;
+	
+	int firstedge = 20;
+	
+	public void update(Keyboard key,GraphicsContext gc) {
+		
+		if (key.left) {
+			shipX--;
+		}
+		if (key.right) {
+			shipX++;
+
+		}
+		
+		
+		if (shipX == edge ) {
+			shipX--;
+
+		}
+				
+		if (shipX == firstedge ) {
+			shipX++;
+
+		}
+		
 	// use axis aligned bounding box 
+	
+	
+	
+	
+}
+
+	
+	
+	
 	
 }
--- src/Window.java
+++ src/Window.java
@@ -6,10 +6,12 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
+//import main.Keyboard;
 
 
 public class Window extends Application {
@@ -23,6 +25,9 @@ public void start(Stage stage) {
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		
     	Scene scene = new Scene(game,300,300,Color.BLACK );//sets Window size
+    	scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> this.key.KeyboardInput(key, gc));
+		scene.addEventHandler(KeyEvent.KEY_RELEASED, (key) -> this.key.KeyboardInput(key, gc));
+		
         stage.setScene(scene);
         stage.setTitle("Asteroids");
         stage.show();
@@ -52,11 +57,13 @@ public void start(Stage stage) {
 			@Override
 			public void handle(long now) {
 
+				
+				
+				
+				gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
 				ship.draw(gc);
+				ship.update(key, gc);
 				
-				
-				
-					
 				}
 
 			}.start();
@@ -64,7 +71,9 @@ public void start(Stage stage) {
         
     } // end start();
     
-	Ship ship = new Ship();
+
+Keyboard key = new Keyboard();
+Ship ship = new Ship();
 
 
 }
